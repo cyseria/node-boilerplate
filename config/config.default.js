cc90aa700cbf6b8eb8b32926c4b64748f1a9c8f5
chenmanqing <chenmanqing@baidu.com>
2018-08-30 16:53:13 +0800

    chore: use egg-int to init project
    Change-Id: I3cc2f4dc2c919ca65d5ab6453ea40a32c237a510

---
+++ config/config.default.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1535617963473_3838';
+
+  // add your config here
+  config.middleware = [];
+
+  return config;
+};
